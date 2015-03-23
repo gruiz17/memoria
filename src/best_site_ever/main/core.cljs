7080366d6efb325534f12c1e6bc374a0a30b5dcd
Gabriel Ruiz <gabe@ggruiz.me>
2015-03-23 00:14:26 -0700

    remove async and goog events from require statements

--- src/best_site_ever/main/core.cljs
+++ src/best_site_ever/main/core.cljs
@@ -1,8 +1,5 @@
 (ns best-site-ever.main.core
-  (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [goog.events :as events]
-            [cljs.core.async :refer [put! chan <!]]
-            [best-site-ever.main.board :as board]
+  (:require [best-site-ever.main.board :as board]
             [best-site-ever.main.keyboard :as keyboard]
             [best-site-ever.main.mouse :as mouse]
             [best-site-ever.main.state :as state]))
--- src/best_site_ever/main/keyboard.cljs
+++ src/best_site_ever/main/keyboard.cljs
@@ -1,4 +1,3 @@
 (ns best-site-ever.main.keyboard
-  (:require [cljs.core.async :refer [put! chan <!]]
-            [best-site-ever.main.state :as state]))
+  (:require [best-site-ever.main.state :as state]))
 (.log js/console "haha")
